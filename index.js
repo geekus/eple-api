35928c9ff59cc3c609f5639c7866aff98cc16a0b
Håvard Ranum <havard.ranum@me.com>
2017-04-01 15:41:01 +0200

    chore(app): add initial app

---
+++ index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.set('json spaces', 2);
+app.set('x-powered-by', false);
+app.set('etag', 'strong');
+
+app.use(bodyParser.json());
+
+app.all('/', function (req, res) {
+  res.json({message: 'Hello, World!'});
+});
+
+app.listen(8080, function () {
+  console.log('API listening on port 8080.');
+});
