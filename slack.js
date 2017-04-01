66fa5ede425d309df60952fb020b7b0b8676fcb1
Håvard Ranum <havard.ranum@me.com>
2017-04-01 20:25:23 +0200

    chore(slack): verify token

--- slack.js
+++ slack.js
@@ -3,8 +3,14 @@
 const express = require('express');
 const router = new express.Router();
 
+const secrets = require('./lib/secrets');
+
 router.use((req, res, next) => {
   if (req.body.challenge) {
+    if (req.body.token !== secrets.SLACK_VERIFICATION_TOKEN) {
+      throw new Error('Unauthorized Slack app tried to use endpoint');
+    }
+
     res.json({challenge: req.body.challenge});
   }
 
